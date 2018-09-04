2a065e2980e5e4a34522be84b4136fe203683aeb
Dan <danny_birch@hotmail.com>
2018-09-04 19:44:31 +0100

    Init commit

---
+++ functions/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    AUTH0_DOMAIN: "code82.auth0.com", // e.g., you.auth0.com
+    AUTH0_API_AUDIENCE: "http://localhost:1337/", // e.g., http://localhost:1337/
+    FIREBASE_KEY:
+        "./firebase/vuejs-83403-firebase-adminsdk-ls936-162b712743.json", // e.g., your-project-firebase-adminsdk-xxxxx-xxxxxxxxxx.json
+    FIREBASE_DB: "https://vuejs-83403.firebaseio.com/" // e.g., https://your-project.firebaseio.com
+};
--- functions/index.js
+++ functions/index.js
@@ -1,53 +1,74 @@
-const admin = require("firebase-admin");
-const cors = require("cors")({ origin: false });
-const auth0 = require("auth0-js");
-const functions = require("firebase-functions");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const config = require("./config");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const firebaseAdmin = require("firebase-admin");
 
-// https://community.auth0.com/t/no-way-to-delegate-firebase-token-with-v8-web-sdk/6904/7
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-var auth0Web = new auth0.WebAuth({
-  domain: "code82.auth0.com",
-  clientID: "lGYegv04mhYn0lTILyPqM9xaIEuNS_DM"
+const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    audience: config.AUTH0_API_AUDIENCE,
+    issuer: `https://${config.AUTH0_DOMAIN}/`,
+    algorithm: "RS256"
 });
 
-var serviceAccount = require("./service-account.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://vuejs-83403.firebaseio.com"
+// Initialize Firebase Admin with service account
+const serviceAccount = require(config.FIREBASE_KEY);
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: config.FIREBASE_DB
 });
 
-exports.delegateToken = functions.https.onRequest((req, res) => {
-  req.set("Access-Control-Allow-Origin", "*");
-  req.set("Access-Control-Allow-Methods", "POST");
-  req.set("Access-Control-Allow-Headers", "*");
-  req.set("Access-Control-Allow-Credentials", "true");
-  // if (req.method === "OPTIONS") {
-  //   res.status(204).send("");
-  // }
-  cors(req, res, () => {
-    let userId = req.body.userId;
-    const accessToken = req.headers.authorization.split("Bearer ")[1];
-
-    auth0Web.client.userInfo(accessToken, (err, user) => {
-      if (err) {
-        console.log(err);
-        res.status(403).send("Unauthorized");
-      } else {
-        if (userId === user.user_id) {
-          admin
-            .auth()
-            .createCustomToken(userId)
-            .then(customToken => {
-              res.send(customToken);
-              return customToken;
-            })
-            .catch(error => {
-              console.log("Error creating custom token:", error);
-            });
-        } else {
-          res.status(403).send("Unauthorized");
-        }
-      }
+module.exports = function (app) {
+    // Auth0 athentication middleware
+    const jwtCheck = jwt({
+        secret: jwks.expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
+        }),
+        audience: config.AUTH0_API_AUDIENCE,
+        issuer: `https://${config.AUTH0_DOMAIN}/`,
+        algorithm: "RS256"
     });
-  });
-});
+
+    // Initialize Firebase Admin with service account
+    const serviceAccount = require(config.FIREBASE_KEY);
+    firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(serviceAccount),
+        databaseURL: config.FIREBASE_DB
+    });
+
+    // GET object containing Firebase custom token
+    app.get("/auth/firebase", jwtCheck, (req, res) => {
+        console.log(req);
+        // Create UID from authenticated Auth0 user
+        const uid = req.user.sub;
+        // Mint token using Firebase Admin SDK
+        firebaseAdmin
+            .auth()
+            .createCustomToken(uid)
+            .then(customToken =>
+                // Response must be an object or Firebase errors
+                res.json({ firebaseToken: customToken })
+            )
+            .catch(err =>
+                res.status(500).send({
+                    message: "Something went wrong acquiring a Firebase token.",
+                    error: err
+                })
+            );
+    });
+};
